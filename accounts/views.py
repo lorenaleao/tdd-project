3772cf8964ec5fc22c39079cd12f97d2e807e960
lorenaleao <lorenalleao@gmail.com>
2021-03-07 00:32:44 -0300

    Usando nosso backend de autenticação na view de login

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from accounts.models import Token
-from django.contrib import messages
+from django.contrib import auth, messages
 from django.core.mail import send_mail
 from django.core.urlresolvers import reverse
 from django.shortcuts import redirect
@@ -24,4 +24,5 @@ def send_login_email(request):
     return redirect('/')
 
 def login(request):
+    auth.authenticate(uid=request.GET.get('token'))
     return redirect('/')
